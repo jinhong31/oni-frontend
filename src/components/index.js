6b1156bff1c6d277923da23d295a311c26198c0f
leopard1218 <snowleopards1218@gmail.com>
2022-09-16 03:55:19 +0300

    test

--- src/components/index.js
+++ src/components/index.js
@@ -24,6 +24,7 @@ const Interface = () => {
   useEffect(() => {
     const jeetTimer = async () => {
       if (jeetState) return;
+      // setCountTime(countTime - 1)
       startTimer(countTime);
     }
     jeetTimer();
@@ -32,15 +33,15 @@ const Interface = () => {
   useEffect(() => {
     const getData = async () => {
       const _data = await axios.get(baseURL);
-      if(tradingState !== _data.data.tradingState) {
+      if (tradingState !== _data.data.tradingState) {
         setTradingState(_data.data.tradingState);
       }
 
-      if(jeetState !== _data.data.jeetState) {
+      if (jeetState !== _data.data.jeetState) {
         setJeetState(_data.data.jeetState);
       }
 
-      if(!jeetState && countTime !== _data.data.countTime) {
+      if (!jeetState) {
         setCountTime(_data.data.countTime);
       }
     }
@@ -74,6 +75,38 @@ const Interface = () => {
         <br />
         <br />
         <br />
+
+        <div style={{ textAlign: "center" }}>
+
+          <div className="state-content">
+            <div className={`state-icon ${jeetState ? "zoom-in-out-circle": ""} ${tradingState ? "background-green " : "background-yellow"}`} />
+            <div className="state-div">
+              {tradingState ? (
+                <>
+                  <p className="state-statement green font-size-35">Trading Is</p>
+                  <p className="state-statement green font-size-35"> Currently Active!</p>
+                </>
+              ) : <p className="state-statement">Coming Soon!</p>}
+            </div>
+          </div>
+          <br />
+          <div className="state-content">
+            <div className={`state-icon background-red ${jeetState ? "" : "zoom-in-out-circle"}`}/>
+            <div className="state-div">
+              <p className="state-statement red font-size-27">Trading Is Temporarily</p><p className="state-statement red font-size-27"> Disabled For:</p>
+              <div className="count-down">
+                <h2>{timer}</h2>
+              </div>
+            </div>
+          </div>
+          <br />
+        </div>
+        <div className="elementor-title-div">
+          <h2 className="elementor-title">If and only if there are to be exactly <font className="red bold">6 sell transactions</font> that occur within no more than <font className="green bold">3 minutes</font> of each other, selling for all holders will be completely disabled for <font class="red bold">9 minutes</font>. After 9 minutes, <font className="green bold">selling will be allowed</font> again, and thus, the cycle will continue <font className="bold">automatically</font> and <font className="bold">autonomously</font>.</h2>
+        </div>
+        <iframe loading="lazy" border="0" style={{ border: "none", borderRadius: "12px", overflow: "hidden", maxWidth: "100%", maxHeight: "760px" }} scrolling="no" src="https://app.uniswap.org/#/swap?theme=dark&amp;use=v2&amp;slippage=10.00&amp;inputCurrency=ETH&amp;outputCurrency=0x0000000" width="1200" height="720"></iframe>
+        <h2 className="elementor-heading-title elementor-size-default">Join The $ONI Community:</h2>
+        <br />
         <div className="social-icons">
           <div className="col-md-4">
             <a className="elementor-icon" href="https://t.me/JeetDemon" target="_blank" style={{ textDecoration: "none" }}>
@@ -92,24 +125,7 @@ const Interface = () => {
           </div>
         </div>
         <br />
-        <br />
-        <h2 className="elementor-heading-title elementor-size-default">Website Opening Soon</h2>
       </div>
-
-      <div style={{ textAlign: "center" }}>
-        <div className="state-content">
-          <div className={`state-icon ${tradingState ? jeetState ? "active-icon" : "inactive-icon" : "before-icon"}`} />
-          <div className="state-div">
-            {tradingState ? jeetState ? <p className="state-statement">Active Selling!</p> : <p className="state-error">Stop Selling!</p> : <p className="state-statement">Coming Soon!</p>}
-          </div>
-        </div>
-        <div className="count-down">
-          {(tradingState && !jeetState) ? (
-            <h2>{timer}</h2>
-          ) : ""}
-        </div>
-      </div>
-
     </>
   );
 }
